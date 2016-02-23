853d8ffd3ffbf280d093e294d6e47d728b4dadf8
eddieebeling <eddie.ebeling@hdsmith.com>
2016-02-23 11:55:08 -0600

    breaking out angular templates

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.watch("./*.html").on('change', browserSync.reload);
 gulp.task('sass', function () {
 return gulp.src("./scss/**/*.scss")
   .pipe(sass())
-  .pipe(gulp.dest('./'))
+  .pipe(gulp.dest('./css/'))
   .pipe(browserSync.stream());
 });
 
